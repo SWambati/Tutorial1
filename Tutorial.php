ce246abc9503f197296649691e5fd72a7c09774b
SWambati <166502884+SWambati@users.noreply.github.com>
2024-08-26 10:15:12 +0300

    Add files via upload

---
+++ Tutorial.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial</title>
+</head>
+<body>
+    <h1> This is a Github tutorial</h1>
+    <?php
+    require_once "Classwork2.php";
+    print $Obj -> user_age("Alexa", 2000);
+    print '<br>';
+    ?>
+</body>
+</html>
